ed7f2f49a713d9c3ea50a28d50f4a80097a75f97
Steve Stedman <steve@stedman.dev>
2020-04-03 10:20:45 -0500

    Fix sponsor style

--- src/components/post-sponsor.jsx
+++ src/components/post-sponsor.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import ExternalLink from './external-link';
 
 const PostSponsor = ({ sponsor }) => (
   <div className="tile">
-    <div className="tile is-child">
-      <h3 className="post-sponsor-title">Sponsor</h3>
-      <div className="post-sponsor-name">Drinks and pizza for this event will be provided by the wonderful folks at <a href={sponsor.url} target="_blank" rel="no-follow">{sponsor.name}</a>.</div>
+    <div className="tile is-child content">
+      <h3>Sponsor</h3>
+      <p>Drinks and pizza for this event will be provided by the wonderful folks at <ExternalLink href={sponsor.url}>{sponsor.name}</ExternalLink>.</p>
       {sponsor.careerUrl
         && (
-          <div className="post-sponsor-career-url">
-            {sponsor.name} is hiring. Check out their <a href={sponsor.careerUrl} target="_blank" rel="no-follow">Career page</a> for more information.
-          </div>
+          <p>
+            {sponsor.name} is hiring. Check out their <ExternalLink href={sponsor.careerUrl}>Career page</ExternalLink> for more information.
+          </p>
         )}
     </div>
   </div>
