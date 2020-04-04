5306a86f452a15b1b709e46474f4e36dfdbff38a
Steve Stedman <steve@stedman.dev>
2020-04-03 23:12:57 -0500

    Make sure careerUrl exists

--- src/components/post-sponsor.jsx
+++ src/components/post-sponsor.jsx
@@ -11,7 +11,7 @@ const PostSponsor = ({ sponsor }) => {
       <div className="tile is-child content">
         <h3>Sponsor</h3>
         <p>Drinks and pizza for this event will be provided by the wonderful folks at <ExternalLink href={biz.url}>{biz.name}</ExternalLink>.</p>
-        {sponsor.hiring
+        {sponsor.hiring && biz.careerUrl
           && (
             <p>
               {biz.name} is hiring. Check out their <ExternalLink href={biz.careerUrl}>Career page</ExternalLink> for more information.
