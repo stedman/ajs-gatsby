dc3e5d52323edc15d4e657ffef228430f4a62f2d
Steve Stedman <steve@stedman.dev>
2020-04-04 15:00:36 -0500

    Fix header image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
+import LogoImage from './logo-image';
 
 const Header = ({ siteTitle }) => (
   <header className="hero is-primary">
@@ -9,9 +10,9 @@ const Header = ({ siteTitle }) => (
         <div className="navbar" role="navigation" aria-label="main navigation">
           <div className="navbar-brand">
             <figure className="image is-96x96">
-              <img src="/icons/icon-96x96.png" alt="" />
+              <LogoImage />
             </figure>
-            <Link to="/" className="navbar-item title has-text-white is-size-1">
+            <Link to="/" className="navbar-item title has-text-white is-size-1-tablet">
               {siteTitle}
             </Link>
           </div>
--- src/components/logo-image.js
+++ src/components/logo-image.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -16,17 +16,17 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "austinjavascript-icon.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+          fixed(height: 96, width: 96) {
+            ...GatsbyImageSharpFixed
           }
         }
       }
     }
-  `)
+  `);
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+  return <Img fixed={data.placeholderImage.childImageSharp.fixed} alt="logo" />;
+};
 
-export default Image
+export default Image;
