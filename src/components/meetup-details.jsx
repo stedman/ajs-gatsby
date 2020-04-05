fe32989280c0f73ae075b832f11ce789831b5141
Steve Stedman <steve@stedman.dev>
2020-04-05 10:40:12 -0500

    Add null checks

--- src/components/meetup-details.jsx
+++ src/components/meetup-details.jsx
@@ -2,40 +2,52 @@ import React from 'react';
 import OrgData from '../content/organizations.yaml';
 
 const MeetupDetails = ({ frontmatter, fields }) => {
-  // TODO: what if there is no venue.id?
-  const venue = OrgData[`${frontmatter.venue.key}`];
-  const after = OrgData[`${frontmatter.after.key}`];
+  const venue = frontmatter && frontmatter.venue && frontmatter.venue.key
+    ? OrgData[`${frontmatter.venue.key}`]
+    : undefined;
+  const after = frontmatter && frontmatter.after && frontmatter.after.key
+    ? OrgData[`${frontmatter.after.key}`]
+    : undefined;
 
   return (
-    <div className="message">
-      <div className="message-header">
-        Meetup
-      </div>
-      <div className="message-body">
-        <div dateTime={fields.date}>
-          DATE <span className="icon has-text-grey-dark"><i className="fas fa-calendar" /></span> <time className="has-text-primary has-text-weight-bold" dateTime={fields.dateShort}>{fields.date}</time>
-        </div>
-        <div>
-          TIME <span className="icon has-text-grey-dark"><i className="fas fa-clock" /></span> <time className="has-text-primary has-text-weight-bold" dateTime="15:30">7:30PM</time> - <time className="has-text-primary has-text-weight-bold" dateTime="21:00">9:00PM</time>
-        </div>
-        <div>
-          LOCATION <span className="icon has-text-grey-dark"><i className="fas fa-map-marker-alt" /></span>
-          <strong className="has-text-primary">{venue.name}</strong>
-          {' '}
-          {venue.location
-            && (
-              <em>({ venue.location})</em>
-            )}
-          <div className="has-offset-top">
-            Afterwards, the discussion carries on at <strong>
-              {after.url
-                ? <a href={after.url} target="_blank" rel="nofollow noopener noreferrer">{after.name}</a>
-                : after.name}
-            </strong> ({after.location}).
+    <>
+      {venue
+        && (
+          <div className="message">
+            <div className="message-header">
+              Meetup
+            </div>
+            <div className="message-body">
+              <div dateTime={fields.date}>
+                DATE <span className="icon has-text-grey-dark"><i className="fas fa-calendar" /></span> <time className="has-text-primary has-text-weight-bold" dateTime={fields.dateShort}>{fields.date}</time>
+              </div>
+              <div>
+                TIME <span className="icon has-text-grey-dark"><i className="fas fa-clock" /></span> <time className="has-text-primary has-text-weight-bold" dateTime="15:30">7:30PM</time> - <time className="has-text-primary has-text-weight-bold" dateTime="21:00">9:00PM</time>
+              </div>
+              <div>
+                LOCATION <span className="icon has-text-grey-dark"><i className="fas fa-map-marker-alt" /></span>
+                <strong className="has-text-primary">{venue.name}</strong>
+                {' '}
+                {venue.location
+                  && (
+                    <em>({ venue.location})</em>
+                  )}
+
+                {after
+                  && (
+                    <div className="has-offset-top">
+                      Afterwards, the discussion carries on at <strong>
+                        {after.url
+                          ? <a href={after.url} target="_blank" rel="nofollow noopener noreferrer">{after.name}</a>
+                          : after.name}
+                      </strong> ({after.location}).
+                    </div>
+                  )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        )}
+    </>
   );
 };
 
--- src/components/post-sponsor.jsx
+++ src/components/post-sponsor.jsx
@@ -3,22 +3,28 @@ import ExternalLink from './external-link';
 import OrgData from '../content/organizations.yaml';
 
 const PostSponsor = ({ sponsor }) => {
-  // TODO: what if there is no sponsor.id? Defaults?
-  const biz = OrgData[`${sponsor.key}`];
+  const biz = sponsor && sponsor.key
+    ? OrgData[`${sponsor.key}`]
+    : undefined;
 
   return (
-    <div className="tile">
-      <div className="tile is-child content">
-        <h3>Sponsor</h3>
-        <p>Drinks and pizza for this event will be provided by the wonderful folks at <ExternalLink href={biz.url}>{biz.name}</ExternalLink>.</p>
-        {sponsor.hiring && biz.careerUrl
-          && (
-            <p>
-              {biz.name} is hiring. Check out their <ExternalLink href={biz.careerUrl}>Career page</ExternalLink> for more information.
-            </p>
-          )}
-      </div>
-    </div>
+    <>
+      {biz
+        && (
+          <div className="tile">
+            <div className="tile is-child content">
+              <h3>Sponsor</h3>
+              <p>Drinks and pizza for this event will be provided by the wonderful folks at <ExternalLink href={biz.url}>{biz.name}</ExternalLink>.</p>
+              {sponsor.hiring && biz.careerUrl
+                && (
+                  <p>
+                    {biz.name} is hiring. Check out their <ExternalLink href={biz.careerUrl}>Career page</ExternalLink> for more information.
+                  </p>
+                )}
+            </div>
+          </div>
+        )}
+    </>
   );
 };
 
